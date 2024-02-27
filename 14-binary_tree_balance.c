164f58712e36d8b6cc6b416c1ed53811884c92dc
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-02-26 23:00:49 -0800

    Fix height function for Balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 size_t binary_tree_height(const binary_tree_t *tree);
 
 
@@ -38,24 +39,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	}
 
-	size_t l_height = 1, r_height = 1;
+	size_t l_height, r_height;
 
-	if (tree->left != NULL)
-	{
-		l_height = 1 + binary_tree_height(tree->left);
-	}
+	l_height = 1 + binary_tree_height(tree->left);
 
-	if (tree->right != NULL)
-	{
-		r_height = 1 + binary_tree_height(tree->right);
-	}
+	r_height = 1 + binary_tree_height(tree->right);
 
-	if (l_height > r_height)
-	{
-		return (l_height);
-	}
-	else
-	{
-		return (r_height);
-	}
+	return (l_height > r_height ? l_height : r_height);
 }
